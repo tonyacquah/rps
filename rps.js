38330955a548cb422dfd82d07e016cfdcdef23e6
Anthony Acquah <acquah.tony@gmail.com>
2023-04-09 23:35:50 +0000

    added scores for each game round in JS, game  description in index file

--- rps.js
+++ rps.js
@@ -8,28 +8,42 @@ const getComputerChoice = () => {
     } else {
         rps = "PAPER"
     }
-    //console.log(rps) //for debugging
+
     return rps
 }
-//getComputerChoice() //for debugging
+
 
 const playRound = playerSelection => {
+    let roundResultStr
+    let roundScorePlayer
+    let roundScoreComputer
+    let roundResultArr = [];
     let computerSelection = getComputerChoice()
     playerSelection = playerSelection.toUpperCase()
-    console.log(computerSelection) //for debugging
-    console.log(playerSelection) //for debugging
+   // console.log(computerSelection) //for debugging
+    //console.log(playerSelection) //for debugging
     if (playerSelection === computerSelection) {
-        return (console.log("Draw: Play again. ", playerSelection, computerSelection))
+        roundResultStr = `Draw: Play again. ${playerSelection} : ${computerSelection}`
+        roundScorePlayer = roundScoreComputer = 0
+
+
     } else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") || //mulltiline if statement containing all the posibilities the ***player wins****
         (playerSelection === "PAPER" && computerSelection === "ROCK") ||
-        (playerSelection === "SCISSORS" && computerSelection === "PAPER")) { //end of if condition begining of function block
-        return (console.log(`\nYou Win! \n${playerSelection} beats ${computerSelection}\n`))
+        (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
+        roundResultStr = `You Win! ${playerSelection} beats ${computerSelection}`
+        roundScorePlayer = 1
+        roundScoreComputer = 0
     }
     else if ((computerSelection === "ROCK" && playerSelection === "SCISSORS") || //mulltiline if statement containing all the posibilities the ***computer wins****
         (computerSelection === "PAPER" && playerSelection === "ROCK") ||
-        (computerSelection === "SCISSORS" && playerSelection === "PAPER")) { //end of if condition begining of function block
-        return (console.log(`\nYou Lose! \n${computerSelection} beats ${playerSelection}\n`))
+        (computerSelection === "SCISSORS" && playerSelection === "PAPER")) {
+        roundResultStr = `You Lose! ${computerSelection} beats ${playerSelection}`
+        roundScorePlayer = 0
+        roundScoreComputer = 1
     }
+    roundResultArr.push(roundResultStr, roundScorePlayer, roundScoreComputer)
+    //console.log(roundResultArr)
+    return roundResultArr
 }
 
 //playRound("SCISSORS"); //for debugging
@@ -48,4 +62,23 @@ const game = () => {
     }
 
 }
+
+/*const game = () => {
+    for (let i = 0; i < 5; i++)
+
+        do {
+            let userSelection
+            let promptText = "Type ROCK, PAPER or SCISSORS"
+            do {
+                userSelection = prompt(promptText)
+                userSelection = userSelection.toUpperCase()
+                promptText = "Invalid input. Check and type again"
+            } while (userSelection != "ROCK" && userSelection != "PAPER" && userSelection != "SCISSORS") //input validationadde
+
+            console.log(playRound(userSelection)); //should happen 5 times
+
+        } while () //until someone gets 5
+
+}*/
+
 game();
